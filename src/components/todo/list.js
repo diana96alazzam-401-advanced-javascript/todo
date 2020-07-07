4ceababe93d0c5aa110bb8a6f8ccae86f2348cdc
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-07-07 09:35:24 +0300

    styling

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,49 +1,38 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-// import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
 
 
 
 function TodoList(props) {
+  console.log(props);
   if (props) {
     if (props.list) {
       return (
-      // <>
-      //   {props.list.map(item => (
-
-      //     <Card
-      //       border="primary"
-      //       style={{ width: '18rem' }}
-      //       className={`complete-${item.complete.toString()}`}
-      //       key={item._id}
-      //       onClick={() => props.handleComplete(item._id)}
-      //     >
-
-      //       <Card.Header>
-      //         <Card.title className="rounded-pill" alt="150x75" variant={(item.complete)?'danger':'success'}>
-      //         </Card.title>
-
-      //       </Card.Header>
-      //       <Card.Body>
-      //         {item.text}
-      //       </Card.Body>
-      //     </Card>
-      //   ))}
-      // </>
-
-
-        <ListGroup>
+        <div>
           {props.list.map(item => (
-            <ListGroup.Item
+            <Modal.Dialog
               variant={(item.complete)?'danger':'success'}
               className={`complete-${item.complete.toString()}`}
               key={item._id}
               onClick={() => props.handleComplete(item._id)}
             >
-              {item.text}
-            </ListGroup.Item>
+              <Modal.Header closeButton>
+                <Button style={{marginRight:'3vw'}} className="rounded-pill" alt="150x75" variant={(item.complete)?'danger':'success'} >{(item.complete)?'complete':'pending'}</Button>
+                <p>{item.assignee}</p>
+              </Modal.Header>
+              <Modal.Body>
+                <p>{item.text}</p>
+              </Modal.Body>
+              <Modal.Footer>
+                <p>Difficulty: {item.difficulty}</p>
+              </Modal.Footer>
+
+            </Modal.Dialog>
           ))}
-        </ListGroup>
+        </div>
       );
     }
   } else {
