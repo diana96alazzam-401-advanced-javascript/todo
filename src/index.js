2c29489b7c93ac004873f4d39e94aec17f4cbdb2
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-07-07 10:23:10 +0300

    deleted scripts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorker from './serviceWorker';
 
 
 import App from './app.js';
@@ -17,5 +16,4 @@ function Main () {
 const rootElement = document.getElementById('root');
 ReactDOM.render(<Main />, rootElement);
 
-serviceWorker.unregister();
 
